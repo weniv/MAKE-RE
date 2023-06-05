8eeda25f4f7a042302aec011838eb2c7b16543b3
ujin16 <94890646+ujin16@users.noreply.github.com>
2023-06-05 17:08:15 +0900

    Feat: 이력서 작성 페이지

---
+++ src/pages/Write.jsx
@@ -0,0 +1,15 @@
+// import Profile from '../components/Profile/Profile'
+// import Intro from '../components/Intro/Intro'
+// import Skills from '../components/Skills/Skills'
+
+function Write() {
+  return (
+    <main>
+      {/* <Profile />
+      <Intro />
+      <Skills /> */}
+    </main>
+  )
+}
+
+export default Write
