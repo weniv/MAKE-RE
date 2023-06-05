ac512db3897ca5b098a08e175de2019424705377
baek gyeonghyeon <baekg.dev@gmail.com>
2023-06-05 17:37:16 +0900

    Feat: Experience 미리보기 화면 마크업 및 스타일링

---
+++ src/components/ExperienceResult/ExperienceResult.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from './experienceResult.module.css'
+
+export default function ExperienceResult() {
+  return (
+    <section className={styles.exp}>
+      <h3 className={styles.title}>Experience</h3>
+      <ul className={styles.list_exp}>
+        <li>
+          <span className={styles.year}>2016</span>
+          <p className={styles.item}>베트남 ICT 해외봉사</p>
+        </li>
+        <li>
+          <span className={styles.year}>2012</span>
+          <p className={styles.item}>베트남 ICT 해외봉사</p>
+        </li>
+        <li>
+          <span className={styles.year}>2019</span>
+          <p className={styles.item}>베트남 ICT 해외봉사</p>
+        </li>
+        <li>
+          <span className={styles.year}>2010</span>
+          <p className={styles.item}>베트남 ICT 해외봉사</p>
+        </li>
+      </ul>
+    </section>
+  )
+}
--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -1,5 +1,12 @@
+import ExperienceResult from '../components/ExperienceResult/ExperienceResult'
+
 function Preview() {
-  return <main className="wrap">미리보기 페이지입니다.</main>
+  return (
+    <main className="wrap">
+      미리보기 페이지입니다.
+      <ExperienceResult />
+    </main>
+  )
 }
 
 export default Preview
