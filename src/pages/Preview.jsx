404e9ff0a22d3aa7100e2fb827082ed1b54ba6a9
ujin16 <94890646+ujin16@users.noreply.github.com>
2023-06-06 18:06:15 +0900

    Feat: SkillsOutput component UI 작성

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -1,11 +1,13 @@
 import Profile from '../components/Profile/ProfileOutput'
 import Intro from '../components/Intro/IntroOutput'
+import Skills from '../components/Skills/SkillsOutput'
 
 function Preview() {
   return (
     <main>
       <Profile />
       <Intro />
+      <Skills />
     </main>
   )
 }
