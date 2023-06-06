e3f4b52a2ffd50c6a06417bc9c456bfb873dccab
ujin16 <94890646+ujin16@users.noreply.github.com>
2023-06-06 17:49:33 +0900

    Feat: IntroOutput component UI 작성

---
+++ src/components/Intro/IntroOutput.jsx
@@ -0,0 +1,23 @@
+import styles from './IntroOutput.module.css'
+
+function IntroOutput() {
+  return (
+    <section className={styles.introSection}>
+      <h2>Introduction</h2>
+      <div className={styles.intro}>
+        지방자치단체는 주민의 복리에 관한 사무를 처리하고 재산을 관리하며,
+        법령의 범위안에서 자치에 관한 규정을 제정할 수 있다. 정당의 설립은
+        자유이며, 복수정당제는 보장된다. 국토와 자원은 국가의 보호를 받으며,
+        국가는 그 균형있는 개발과 이용을 위하여 필요한 계획을 수립한다.
+        민주평화통일자문회의의 조직·직무범위 기타 필요한 사항은 법률로 정한다.
+        지방자치단체는 주민의 복리에 관한 사무를 처리하고 재산을 관리하며,
+        법령의 범위안에서 자치에 관한 규정을 제정할 수 있다. 정당의 설립은
+        자유이며, 복수정당제는 보장된다. 국토와 자원은 국가의 보호를 받으며,
+        국가는 그 균형있는 개발과 이용을 위하여 필요한 계획을 수립한다.
+        민주평화통일자문회의의 조직·직무범위 기타 필요한 사항은 법률로 정한다.
+      </div>
+    </section>
+  )
+}
+
+export default IntroOutput
--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -1,9 +1,11 @@
 import Profile from '../components/Profile/ProfileOutput'
+import Intro from '../components/Intro/IntroOutput'
 
 function Preview() {
   return (
     <main>
       <Profile />
+      <Intro />
     </main>
   )
 }
