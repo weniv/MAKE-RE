585f585be38570e13186ed59df04626e6f9fc081
min-bok <leehy0782@gmail.com>
2023-06-07 17:38:34 +0900

    refactor: ProjectInput 마크업 수정

--- src/components/Project/ProjectInput.jsx
+++ src/components/Project/ProjectInput.jsx
@@ -1,61 +1,6 @@
 import React from 'react'
-import styles from './ProjectInput.module.css'
-import { ProjectAlign, ProjectHeader, ProjectTitle } from './components/Inputs'
+import Inputs from './components/Inputs'
 
 export default function ProjectInput() {
-  const handleOutline = (e) => {
-    console.log(e.target.value)
-  }
-
-  return (
-    <main>
-      <ProjectAlign />
-      <div className={styles.cont}>
-        <ProjectHeader />
-        {/* 프로젝트 입력창 */}
-        <form>
-          <ProjectTitle />
-
-          {/* <h3>프로젝트 개요</h3>
-          <textarea
-            placeholder="본문 내용 입력"
-            onChange={handleOutline}
-          ></textarea>
-
-          <h3>개발 인원 및 기간</h3>
-          <div className={styles.people}>
-            <h4>인원</h4>
-            <input type="text" placeholder="예) Front-End 4명, Back-End 2명" />
-          </div>
-          <div className={styles.period}>
-            <h4>기간</h4>
-            <input type="month" />
-            <p>~</p>
-            <input type="month" />
-            <input type="checkbox" name="Proceeding" id="Proceeding" />
-            <p>진행중</p>
-          </div>
-
-          <h3>기여 부분</h3>
-          <input
-            type="text"
-            placeholder="예) 스마트 컨트렉스 서버와 연동되는 웹 개발 전반"
-          />
-          <button type="button" className="skillBtn ">
-            +) 추가 입력하기
-          </button>
-          <h3>적용 기술</h3>
-          <input type="text" placeholder="예) Java" />
-          <button type="button" className="skillBtn ">
-            +) 추가 입력하기
-          </button>
-          <h3>깃허브 링크</h3>
-          <input type="text" ee />
-
-          <h3>프로젝트 링크</h3>
-          <input type="text" ee /> */}
-        </form>
-      </div>
-    </main>
-  )
+  return <Inputs />
 }
--- src/components/Project/components/Inputs.jsx
+++ src/components/Project/components/Inputs.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 import styles from '../ProjectInput.module.css'
 
-export const ProjectAlign = () => {
+export default function Inputs() {
+  const handleOutline = (e) => {
+    console.log(e.target.value)
+  }
+
+  return (
+    <main>
+      <ProjectAlign />
+      <div className={styles.cont}>
+        <ProjectHeader />
+        <form>
+          <ProjectTitle />
+          <ProjectOutline />
+          <ProjectDetail />
+          <ProjectContribution />
+          <ProjectSkill />
+          <GithubLink />
+          <DeployLink />
+        </form>
+      </div>
+    </main>
+  )
+}
+
+const ProjectAlign = () => {
   return (
     <>
       <h2>Project</h2>
@@ -13,22 +37,120 @@ export const ProjectAlign = () => {
   )
 }
 
-export const ProjectHeader = () => {
+const ProjectHeader = () => {
   return (
     <div className={styles.projectHeader}>
       <button type="button" className={styles.menuBtn}></button>
-      <h3>AT(Arzra Takarsen)</h3>
+      <p>AT(Arzra Takarsen)</p>
       <button type="button" className={styles.downBtn}></button>
       <button type="button" className={styles.moreBtn}></button>
     </div>
   )
 }
 
-export const ProjectTitle = () => {
+const ProjectTitle = () => {
   return (
     <div className={styles.projectTitle}>
       <h4>프로젝트명 입력</h4>
       <input type="text" placeholder="프로젝트명 입력" />
     </div>
+  )
+}
+
+const ProjectOutline = () => {
+  return (
+    <>
+      <h3>프로젝트 개요</h3>
+      <textarea placeholder="본문 내용 입력"></textarea>
+    </>
+  )
+}
+
+const People = () => {
+  return (
+    <div className={styles.peple}>
+      <h4>인원</h4>
+      <input type="text" placeholder="예) Front-End 4명, Back-End 2명" />
+    </div>
+  )
+}
+
+const Period = () => {
+  return (
+    <div className={styles.period}>
+      <h4>기간</h4>
+      <div>
+        <input type="text" placeholder="YYYY" className={styles.year} />
+        <input type="text" placeholder="MM" className={styles.month} />
+        <input type="text" placeholder="YYYY" className={styles.year} />
+        <input type="text" placeholder="MM" className={styles.month} />
+        <input type="checkbox" name="Proceeding" id="Proceeding" />
+      </div>
+    </div>
+  )
+}
+
+const ProjectDetail = () => {
+  return (
+    <>
+      <h3>개발 인원 및 기간</h3>
+      <People />
+      <Period />
+    </>
+  )
+}
+
+const ProjectContribution = () => {
+  return (
+    <>
+      <h3>기여 부분</h3>
+      <div className={styles.contribution}>
+        <input
+          type="text"
+          placeholder="예) 스마트 컨트렉스 서버와 연동되는 웹 개발 전반"
+        />
+        <button type="button" className="skillBtn ">
+          +) 추가 입력하기
+        </button>
+      </div>
+    </>
+  )
+}
+
+const ProjectSkill = () => {
+  return (
+    <>
+      <h3>적용 기술</h3>
+      <div className={styles.skill}>
+        <input type="text" placeholder="예) Java" />
+        <button type="button" className="skillBtn ">
+          +) 추가 입력하기
+        </button>
+      </div>
+    </>
+  )
+}
+
+const GithubLink = () => {
+  return (
+    <>
+      <h3>깃허브 링크</h3>
+      <div className={styles.github}>
+        <img src="" alt="" />
+        <input type="url" />
+      </div>
+    </>
+  )
+}
+
+const DeployLink = () => {
+  return (
+    <>
+      <h3>프로젝트 링크</h3>
+      <div className={styles.deploy}>
+        <img src="" alt="" />
+        <input type="url" />
+      </div>
+    </>
   )
 }
