7435b971a55c47931042277b2a36f16ab5641cab
min-bok <leehy0782@gmail.com>
2023-06-08 00:33:17 +0900

    fix: pull conflict 해결

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,29 +1,19 @@
-<<<<<<< HEAD
-// import Profile from '../components/Profile/Profile'
-// import Intro from '../components/Intro/Intro'
-// import Skills from '../components/Skills/Skills'
-import ProjectInput from '../components/Project/ProjectInput'
-import CareerInput from '../components/Career/CareerInput'
-=======
 import Profile from '../components/Profile/ProfileInput'
 import Intro from '../components/Intro/IntroInput'
 import Skills from '../components/Skills/SkillsInput'
 import Experience from '../components/Experience/ExperienceInput'
->>>>>>> 004fe9dd9dda918326fc85bacb4e6affc7e69f76
+import ProjectInput from '../components/Project/ProjectInput'
+import CareerInput from '../components/Career/CareerInput'
 
 function Write() {
   return (
     <main>
       <Profile />
       <Intro />
-<<<<<<< HEAD
-      <Skills /> */}
-      <ProjectInput />
-      <CareerInput />
-=======
       <Skills />
       <Experience />
->>>>>>> 004fe9dd9dda918326fc85bacb4e6affc7e69f76
+      <ProjectInput />
+      <CareerInput />
     </main>
   )
 }
