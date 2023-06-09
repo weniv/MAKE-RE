75b96eb654f6950ed45d968dd05a920c71381ec4
imzero9 <skdud000322@naver.com>
2023-06-09 17:30:17 +0900

    Style: fixed 헤더 스타일 추가

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -24,54 +24,74 @@ function App() {
     localStorage.setItem('data', JSON.stringify(resumeData))
   }
 
-  // console.log(resumeData)
-
-  return (
-    <div className={`${App} ${styles.pageWrap}`}>
-      {isWrite ? (
-        <>
-          <header className={styles.headerWrap}>
-            <h1>
-              메이커리
-              <span> Make A Career</span>
-            </h1>
-            <button
-              className={`${styles.header} ${styles.saveBtn}`}
-              onClick={dataUpdateHandler}
-            >
-              임시저장
-            </button>
-            <button
-              className={styles.prevBtn}
-              onClick={() => {
-                setIsWrite(false)
-              }}
-            >
-              미리보기
-            </button>
-            <button className={styles.exportBtn}>PDF로 내보내기</button>
-            <div className={styles.line}></div>
-          </header>
-          <Write setResumeData={setResumeData} resumeData={resumeData} />
-        </>
-      ) : (
-        <>
-          <header className={styles.headerWrap}>
-            <button
-              className={styles.prevBtn}
-              onClick={() => {
-                setIsWrite(true)
-              }}
-            >
-              돌아가기
-            </button>
-            <button className={styles.exportBtn}>PDF로 내보내기</button>
-            <div className={styles.line}></div>
-          </header>
-          <Preview resumeData={resumeData} />
-        </>
-      )}
-    </div>
+  return isWrite ? (
+    <>
+      <header className={styles.headerWrap}>
+        <h1 className={`${styles.logoWrap} ${styles.title}`}>
+          <img src="/images/logo.svg" alt="" />
+          메이커리
+          <span> Make A Career</span>
+        </h1>
+        <nav className={styles.headerNav}>
+          <ul>
+            <li>
+              <button
+                className={`${styles.header} ${styles.saveBtn}`}
+                onClick={dataUpdateHandler}
+              >
+                임시저장
+              </button>
+            </li>
+            <li>
+              <button
+                className={styles.prevBtn}
+                onClick={() => {
+                  setIsWrite(false)
+                }}
+              >
+                미리보기
+              </button>
+            </li>
+            <li>
+              <button className={styles.exportBtn}>PDF로 내보내기</button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className={`${App} ${styles.pageWrap}`}>
+        <Write setResumeData={setResumeData} resumeData={resumeData} />
+      </div>
+    </>
+  ) : (
+    <>
+      <header className={styles.headerWrap}>
+        <h1 className={`${styles.logoWrap} ${styles.title}`}>
+          <img src="/images/logo.svg" alt="" />
+          메이커리
+          <span> Make A Career</span>
+        </h1>
+        <nav className={styles.headerNav}>
+          <ul>
+            <li>
+              <button
+                className={styles.prevBtn}
+                onClick={() => {
+                  setIsWrite(true)
+                }}
+              >
+                돌아가기
+              </button>
+            </li>
+            <li>
+              <button className={styles.exportBtn}>PDF로 내보내기</button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className={styles.prevWrap}>
+        <Preview resumeData={resumeData} />
+      </div>
+    </>
   )
 }
 
