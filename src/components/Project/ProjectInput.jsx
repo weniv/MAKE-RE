98c17eeada4a6ab9008fc77d30c97b9655aae21a
baek gyeonghyeon <baekg.dev@gmail.com>
2023-06-15 13:25:14 +0900

    test2

--- src/components/Project/ProjectInput.jsx
+++ src/components/Project/ProjectInput.jsx
@@ -185,7 +185,6 @@ function ProjectContent({
               value={pro.title}
               onChange={(e) => handleUpdate(idx, e)}
             />
-            t
           </div>
           <div className={styles.contOutline}>
             <h4 className={styles.subTit}>프로젝트 개요</h4>
