a0e99ad89fbe27f057a3cbaf87797de00f00f7b6
baek gyeonghyeon <baekg.dev@gmail.com>
2023-06-22 13:53:49 +0900

    Feat: Footer 항목 추가

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -97,20 +97,6 @@ function App() {
                 미리보기
               </button>
             </li>
-            <li>
-              <a
-                href="https://paullabworkspace.notion.site/Figma-bfa32213fc244db9b31bb8486a479ee6?pvs=4"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Figma 이력서
-              </a>
-            </li>
-            <li>
-              <a href="/files/제코베_포트폴리오_템플릿_배포용.pptx" download>
-                PPT 포트폴리오
-              </a>
-            </li>
           </ul>
         </nav>
       </header>
@@ -121,6 +107,33 @@ function App() {
           setFormNum={setFormNum}
         />
       </div>
+      <footer>
+        <ul className={styles.footerCont}>
+          <li>
+            <p>위니브</p>
+            <a href="https://paullab.co.kr/about.html">회사 소개</a>
+            <a href="https://paullab.co.kr/index.html">제주코딩베이스캠프</a>
+          </li>
+          <li>
+            <p>메이커리</p>
+            <a href="#">메이커리 서비스 소개</a>
+          </li>
+          <li>
+            <p>자료</p>
+            <a
+              href="https://paullabworkspace.notion.site/Figma-bfa32213fc244db9b31bb8486a479ee6?pvs=4"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Figma 이력서 바로가기
+            </a>
+            <a href="/files/제코베_포트폴리오_템플릿_배포용.pptx" download>
+              PPT 포트폴리오 다운로드
+            </a>
+          </li>
+        </ul>
+        <address className={styles.writeCopy}>© Weniv Corp.</address>
+      </footer>
     </>
   ) : (
     <>
@@ -152,6 +165,9 @@ function App() {
       <div className={styles.prevWrap}>
         <Preview resumeData={resumeData} componentRef={componentRef} />
       </div>
+      <footer className={styles.prevFooter}>
+        <address>© Weniv Corp.</address>
+      </footer>
     </>
   )
 }
